2574dda80c9e1d04819a7a60672ea2380f87c5fc
Ales Krestan <krestan.ales@feg.eu>
2024-01-26 11:10:29 +0100

    Redis client, lombok and logback

---
+++ src/main/java/akr/redis/codemonstur/test/RedisClientApp.java
@@ -0,0 +1,36 @@
+package akr.redis.codemonstur.test;
+
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.embedded.Redis;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+@Slf4j
+public class RedisClientApp {
+
+    public static void main(String[] args) throws IOException {
+
+        RedisServer redisServer = startEmbeddedRedis();
+        redisServer.start();
+
+        try (Jedis jedis = getJedis()) {
+            String key = "dog";
+            jedis.set(key, "Spot");
+            log.info("-------------- found {} - {}", key, jedis.get(key));
+        }
+
+        redisServer.stop();
+
+    }
+
+    private static RedisServer startEmbeddedRedis() throws IOException {
+        return new RedisServer(Redis.DEFAULT_REDIS_PORT);
+    }
+
+    private static Jedis getJedis() {
+        return new JedisPool("localhost", 6379).getResource();
+    }
+}
